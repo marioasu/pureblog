914dccd0ba1461eb7b9a8cb17e211299e8ba6fec
suzhongping <suzhongping@phoenixos.com>
2017-08-31 17:04:52 +0800

    add host

--- main.py
+++ main.py
@@ -55,4 +55,4 @@ def post(article):
     return render_template('post.html', posts=posts, curr_post=curr_post)
 
 if __name__ == '__main__':
-    app.run(port=8077)
+    app.run(host='0', port=8077)
